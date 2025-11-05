088460c2ad230b76dcbd99a5cf8aca48b1b39bd3
Dimitre <dimitre@users.noreply.github.com>
2025-11-05 13:06:18 -0300

    scripts to change to static IPs

---
+++ ip/3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+sudo nmcli con mod "Wired connection 1" ipv4.addresses 10.1.91.103/24
+sudo nmcli con mod "Wired connection 1" ipv4.gateway 10.1.91.1
+sudo nmcli con mod "Wired connection 1" ipv4.dns "10.1.91.1 8.8.8.8"
+sudo nmcli con mod "Wired connection 1" ipv4.method manual
+sudo nmcli con up "Wired connection 1"
